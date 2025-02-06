124d775162ed42de3f210b2166c21e80d5059813
Robert <robert.alejandro.manotas@gmail.com>
2025-02-06 18:40:15 -0500

    get started responsive completed

--- app/src/components/GetStartedContainer.jsx
+++ app/src/components/GetStartedContainer.jsx
@@ -4,9 +4,7 @@ import CurvyPicture from "./CurvyPicture";
 export const GetStartedContainer = () => {
   return (
     <section className="get-started-container">
-      <h1>
-        All your files in one secure <br /> location, accessible anywhere.
-      </h1>
+      <h1>All your files in one secure location, accessible anywhere.</h1>
       <p>
         Fylo stores all your most important files in one secure location. Access
         them wherever you need, share and collaborate with friends, family, and
