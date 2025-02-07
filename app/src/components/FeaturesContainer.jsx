eb147f0256a0803fcf32ff9c81f1dfb2beac187c
Robert <robert.alejandro.manotas@gmail.com>
2025-02-06 21:21:37 -0500

    Features component updated

--- app/src/components/FeaturesContainer.jsx
+++ app/src/components/FeaturesContainer.jsx
@@ -3,9 +3,9 @@ import featuresData from "../helpers/featuresData";
 
 export const FeaturesContainer = () => {
   return (
-    <section>
+    <section className="features-container">
       {featuresData.map((feature) => (
-        <div>
+        <div className="features-card">
           <img src={feature.icon} key={feature.id} alt={feature.title} />
           <h3>{feature.title}</h3>
           <p>{feature.description}</p>
